0a7db8b64c69a10f405862b8b0016f00d4997906
Simon Roy <simon.roy1211@gmail.com>
2022-05-19 04:22:09 -0400

    Core cleanup

--- src/notion/core.clj
+++ src/notion/core.clj
@@ -1,12 +1,3 @@
-; I want to make a notion/github bot that will listen for new issues assinged to
-; me from X repo (In my case it would be for Nagano first). The smart thing would
-; probably be to use github webhooks with the notion integration we already have.
-
-; When a new issue is assigned, we add a new element to a kanban datatabase. We
-; Set the titles, links and that's all. The rest is to be set manually.
-
-; token expires in 30 days - 12/04/2022
-; curl -i -u PenguinboiBoi12:$token https://api.github.com/repos/semiweb/nagano/issues
 (ns notion.core
   (:require
     [clojure.edn :as edn]
@@ -14,7 +5,8 @@
     [notion.api.user :as user])
   (:gen-class))
 
+(def client (client/init!))
+
 (defn -main [& args]
-  (let [client (client/init!)
-        user (user/get-user client "4175e406-b678-4e16-9988-c866583daaa6")]
-    (:name user)))
+  (let [user (client/user client "4175e406-b678-4e16-9988-c866583daaa6")]
+    (println user)))
