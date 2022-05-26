e9c586be19273f9ddd64bf96477e60d6cbc719f1
Simon Roy <simon.roy1211@gmail.com>
2022-05-26 02:02:48 -0400

    Adjusted user record

--- src/notion/types/user.clj
+++ src/notion/types/user.clj
@@ -1,7 +1,12 @@
 (ns notion.types.user)
 
+(def bot? (partial = "bot"))
+
 (defrecord User [id name email avatar bot?])
 
+(defn make-user [id name email avatar bot?]
+    (->User id name email avatar bot?))
+
 (defn build-user [m]
-  (let [bot? (= (:type m) "bot")]
-    (->User (:id m) (:name m) (:email (:person m)) (:avatar_url m) bot?)))
+  (let [bot? (bot? (:type m))]
+    (make-user (:id m) (:name m) (:email (:person m)) (:avatar_url m) bot?)))
