fc652434abed07f4efa9f0e95d9d5a73f3c04159
Simon Roy <simon.roy1211@gmail.com>
2022-05-26 02:02:38 -0400

    Added database and page.

--- src/notion/types/database.clj
+++ src/notion/types/database.clj
@@ -1,20 +1,32 @@
 (ns notion.types.database
   (:require [clojure.set :refer [rename-keys]]))
 
-(defonce database-keys {:id :id
-                        :title :title
-                        :icon :icon
-                        :cover :cover
-                        :parent :parent
-                        :properties :properties
-                        :archived :archived?
-                        :created_time :created-time
-                        :created_by :created-by
-                        :last_edited_time :last-edited-time
-                        :last_edited_by :last-edited-by})
+(defn- format-map [m]
+  (let [database-keys {:id :id
+                       :created_time :created-time
+                       :created_by :created-by-id
+                       :last_edited_time :last-edited-time
+                       :last_edited_by :last-edited-by-id
+                       :title :title
+                       :icon :icon
+                       :cover :cover
+                       ; :properties :properties
+                       :parent :parent-id
+                       :url :url
+                       :archived :archived?}]
+    (-> m
+      (select-keys (keys database-keys))
+      (rename-keys database-keys))))
 
-(defrecord Database [id title icon cover parent archived? created-time
-                     created-by last-edited-time last-edited-by url])
+(defrecord Database [id title icon cover parent-id archived? created-time
+                     created-by-id last-edited-time last-edited-by-id url])
 
+; Add the parent, created-by and last-edited-by type?
 (defn build-database [m]
-  (rename-keys m database-keys))
+  (-> (format-map m)
+    (update :title #(:content (:text (first %))))
+    (update :icon :emoji)
+    (update :created-by-id :id)
+    (update :last-edited-by-id :id)
+    (update :parent-id :page_id)
+    (map->Database)))
--- src/notion/types/page.clj
+++ src/notion/types/page.clj
@@ -1,6 +1,12 @@
 (ns notion.types.page)
 
-(defrecord Page [hello])
+(defrecord Page [id created-time created-by-id last-edited-time last-edited-by-id
+                 archived? icon cover properties parent-id url])
+
+(defn make-page [id created-time created-by-id last-edited-time last-edited-by-id
+                 archived? icon cover properties parent-id url]
+  (->Page id created-time created-by-id last-edited-time last-edited-by-id
+          archived? icon cover properties parent-id url))
 
 (defn build-page [m]
   m)
