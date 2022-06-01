8ff867541569e05d91dc1b87d3e8310ec588a490
Simon Roy <simon.roy1211@gmail.com>
2022-06-01 00:40:27 -0400

    Added page construction

--- src/notion/types/page.clj
+++ src/notion/types/page.clj
@@ -1,12 +1,42 @@
-(ns notion.types.page)
+(ns notion.types.page
+  (:require [clojure.set :refer [rename-keys]]))
+
+(defn- format-map [m]
+  (let [database-keys {:id :id
+                       :created_time :created-time
+                       :created_by :created-by-id
+                       :last_edited_time :last-edited-time
+                       :last_edited_by :last-edited-by-id
+                       :title :title
+                       :icon :icon
+                       :cover :cover
+                       ; :properties :properties
+                       :parent :parent-id
+                       :parent-type :parent-type
+                       :url :url
+                       :archived :archived?}]
+    (-> m
+      (select-keys (keys database-keys))
+      (rename-keys database-keys))))
+
+; put this somewhere reusable
+(def types-by-key {:page_id :page,
+                   :database_id :database,
+                   :workspace_id :workspace})
+
 
 (defrecord Page [id created-time created-by-id last-edited-time last-edited-by-id
                  archived? icon cover properties parent-id parent-type url])
 
-(defn make-page [id created-time created-by-id last-edited-time last-edited-by-id
-                 archived? icon cover properties parent-id parent-type url]
-  (->Page id created-time created-by-id last-edited-time last-edited-by-id
-          archived? icon cover properties parent-id parent-type url))
-
+; Add the parent, created-by and last-edited-by type?
 (defn build-page [m]
-  m)
+ (def parent-type (keyword (:type (:parent m))))
+
+ (-> (format-map m)
+   (update :title #(:content (:text (first %))))
+   (update :icon :emoji)
+   (update :created-by-id :id)
+   (update :last-edited-by-id :id)
+   (update :parent-id parent-type)
+   (assoc :parent-type (parent-type types-by-key))
+   (map->Page)))
