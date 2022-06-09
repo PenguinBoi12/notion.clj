11ae80e38e283c46aede72dca95e4ba8538117d9
Simon Roy <simon.roy1211@gmail.com>
2022-06-08 23:59:05 -0400

    Core cleanup --- NONE

--- src/notion/core.clj
+++ src/notion/core.clj
@@ -1,12 +1,8 @@
 (ns notion.core
   (:require
-    [clojure.edn :as edn]
     [notion.client :as client])
   (:gen-class))
 
-(def client (client/init!))
-
-(defn -main [& args]
-  (let [db (client/database client "72fed03b27ca48278f41000c4920afe9")]
-    (println (client/parent client db) )))
-(-main)
+(defn -main
+  "Nothing for now"
+  [& args])
