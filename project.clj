18673f81c57a52ac985cd3d27888c9b20eeeccc1
Simon Roy <simon.roy1211@gmail.com>
2022-06-09 00:00:18 -0400

    Added target path in project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject notion.clj/notion.clj "0.0.1-SNAPSHOT"
-  :description "A clojure library for creating Notion"
+  :description "A notion api wrapper in Clojure"
   :url "https://github.com/penguinboi/notion.clj"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
@@ -13,5 +13,6 @@
                  [integrant/repl "0.3.2"]
                  [nrepl/nrepl "0.8.3"]]
   :main ^:skip-aot notion.core
+  :target-path "target/%s"
   :source-paths ["src"]
-  :profiles {:examples {:source-paths ["examples"]}})
+  :profiles {:uberjar {:aot :all}})
