71c0b7bd1de2d65a1129a48dd6b04f48e4023ebe
Simon Roy <simon.roy1211@gmail.com>
2022-07-13 02:38:26 -0400

    Fix typo

--- src/notion/block.clj
+++ src/notion/block.clj
@@ -1,13 +1,11 @@
 (ns notion.block
   (:require
     [notion.api.utils :as api]
-    [notion.types.block :refer [build-block]))
+    [notion.types.block :refer [build-block]]))
 
-(defn create! [client block]
-  )
+(defn create! [client block])
 
-(defn save! [client block]
-  )
+(defn save! [client block])
 
 (defn delete! [client block]
   (let [block_id (:id block)]
