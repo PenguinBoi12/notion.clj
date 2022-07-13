6f52fcc0017e703f272107c8b59fbcf8566deaa4
Simon Roy <simon.roy1211@gmail.com>
2022-07-13 02:22:00 -0400

    Added block.clj for interaction

---
+++ src/notion/block.clj
@@ -0,0 +1,14 @@
+(ns notion.block
+  (:require
+    [notion.api.utils :as api]
+    [notion.types.block :refer [build-block]))
+
+(defn create! [client block]
+  )
+
+(defn save! [client block]
+  )
+
+(defn delete! [client block]
+  (let [block_id (:id block)]
+    (api/delete! client :block block_id)))
