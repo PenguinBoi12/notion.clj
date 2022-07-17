9a6a23f0acca9901f52e45693837878b07145d61
Simon Roy <simon.roy1211@gmail.com>
2022-07-17 04:07:02 -0400

    Moved some function into notion core

--- src/notion/core.clj
+++ src/notion/core.clj
@@ -1,6 +1,25 @@
 (ns notion.core
+  (:require
+    [notion.api.user :as user]
+    [notion.api.page :as page]
+    [notion.api.block :as block]
+    [notion.api.database :as database])
   (:gen-class))
 
-(defn -main
-  "Nothing for now"
-  [& args])
+(defn parent
+  "Returns the parent of the given object"
+  [client object]
+  (let [id (:parent-id object),
+        type (:parent-type object)
+        func (type {:page page/find, :database database/find})]
+    (func client id)))
+
+(defn created-by
+  "Returns the user who created the object"
+  [client object]
+  (user/find client (:created-by-id object)))
+
+(defn last-edited-by
+  "Returns the last user who edited the object"
+  [client object]
+  (user/find client (:last-edited-by-id object)))
